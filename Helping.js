00e496dd7141ee78d37ed32665a5545372dae55b
Ananthu T Mani <ananthum1999@gmail.com>
2020-05-07 17:04:16 +0530

    code test TKMCE Exuro

---
+++ Helping.js
@@ -0,0 +1,9 @@
+const head1 =  document.querySelector("#h11");
+const head2 =  document.querySelector("#h22");
+
+head1.classList.remove('d-none');
+head1.classList.add('animated', 'fadeInUp', 'delay-1s');
+head1.addEventListener('animationend', function() { 
+  head2.classList.remove('d-none'); 
+  head2.classList.add('animated', 'fadeInUp'); 
+});
---
+++ README.md
@@ -0,0 +1,2 @@
+# ananthutmaniexuro20webcrafts
+A repo created for the TKMCE event EXURO 2020 WebCrafts
