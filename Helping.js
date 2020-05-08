a50ad463c2abd95747e9a51091ce4bef689dcdae
Ananthu T Mani <ananthum1999@gmail.com>
2020-05-08 14:33:57 +0530

    java

--- Helping.js
+++ Helping.js
@@ -11,4 +11,10 @@ head1.addEventListener('animationend', function() {
 var navba = document.getElementById("navbar");
       window.addEventListener("load", function(){
           navba.classList.remove("d-none");
+          head1.classList.remove('d-none');
+          head1.classList.add('animated', 'fadeInUp', 'delay-1s');
+          head1.addEventListener('animationend', function() { 
+            head2.classList.remove('d-none'); 
+            head2.classList.add('animated', 'fadeInUp'); 
+          });
       });
